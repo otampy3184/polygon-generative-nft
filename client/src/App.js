37d5707f1421356e88e08c9e61b63989b7fb2b29
django_freeman <o.tampodo.t.18@gmail.com>
2022-10-23 20:47:28 +0900

    フロントからNFTをMint

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import abi from "./abi/NFTCollectible.json";
 import './App.css';
+
+const contractAddress = "0x0Fd7714EE72Bde2F11525E0069169fAbd990e7A4";
+const contractABI = abi.abi;
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState("");
 
-  const checkIfWalletIsConnected = async() => {
+  const checkIfWalletIsConnected = async () => {
     try {
-      const {ethereum} = window;
-      if(!ethereum){
+      const { ethereum } = window;
+      if (!ethereum) {
         console.log("Make sure you have Metamask");
         return;
       } else {
@@ -16,14 +20,14 @@ function App() {
       }
 
       const accounts = await ethereum.request({ method: "eth_accounts" });
-      if(accounts.length !== 0){
+      if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
         setCurrentAccount(account);
       } else {
         console.log("No authorized account found");
       }
-    } catch(error){
+    } catch (error) {
       console.log(error);
     }
   };
@@ -32,7 +36,7 @@ function App() {
     checkIfWalletIsConnected();
   }, []);
 
-  const connectWallet = async() => {
+  const connectWallet = async () => {
     try {
       const { ethereum } = window;
       if (!ethereum) {
@@ -43,14 +47,57 @@ function App() {
         method: "eth_requestAccounts",
       });
       console.log("Connected:")
-    } catch(error){
+    } catch (error) {
       console.log(error);
     }
   }
+  
+  const mintNftHandler = async() => {
+    try {
+      const { ethereum } = window;
+
+      if(ethereum){
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("initialize payment");
+        let nftTxn = await nftContract.mintNFTs(1, {
+          value: ethers.utils.parseEther("0.01")
+        });
+
+        console.log("Minting...");
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: ${nftTxn.hash}`);
+      }else {
+        console.log("Ethereum object does not exist");
+      }
+    } catch (err){
+      console.log(err);
+    }
+  };
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className="dappButton">
+        Mint NFT
+      </button>
+    );
+  };
 
   return (
     <div className="App">
-      Hello world
+      <div>
+        {!currentAccount && (
+          <button className="dappButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+        {currentAccount && (
+            <mintNftButton />
+        )}
+      </div>
     </div>
   );
 }
